440713ad340629293cbdeef301b4d24a23b4def8
Deepak Chowdary <deepakchowdary866@gmail.com>
2023-09-20 23:19:08 +0530

    showing profiles

--- src/Screens/FindSpecialist.tsx
+++ src/Screens/FindSpecialist.tsx
@@ -1,163 +1,108 @@
-import React from 'react'
+import React, { useState } from 'react'
+import profile from '../Images/profile.png'
 import bgimage from '../Images/findspecialistbackground.jpg'
 import Chip from '@mui/material/Chip';
 import Autocomplete from '@mui/material/Autocomplete';
 import TextField from '@mui/material/TextField';
 import Stack from '@mui/material/Stack';
 import '../Styles/FindSpecialist.css'
+import { Button, Card , CardContent, Typography } from '@mui/material';
+import { FaSearch } from 'react-icons/fa';
 
+interface Profile {
+  id: number;
+  name: string;
+  description: string;
+  photo: string; // Assuming 'profile' is a string URL
+}
 
 const top100Films = [
-    { title: 'The Shawshank Redemption', year: 1994 },
-    { title: 'The Godfather', year: 1972 },
-    { title: 'The Godfather: Part II', year: 1974 },
-    { title: 'The Dark Knight', year: 2008 },
-    { title: '12 Angry Men', year: 1957 },
-    { title: "Schindler's List", year: 1993 },
-    { title: 'Pulp Fiction', year: 1994 },
+    { title: 'python'},
+    { title: 'c++'},
+    { title: 'dot net'},
+    { title: 'JavaScript'},
+    { title: 'Java'},
+    { title: "Php"},
+    { title: 'Sql'},
     {
-      title: 'The Lord of the Rings: The Return of the King',
-      year: 2003,
-    },
-    { title: 'The Good, the Bad and the Ugly', year: 1966 },
-    { title: 'Fight Club', year: 1999 },
-    {
-      title: 'The Lord of the Rings: The Fellowship of the Ring',
-      year: 2001,
-    },
-    {
-      title: 'Star Wars: Episode V - The Empire Strikes Back',
-      year: 1980,
-    },
-    { title: 'Forrest Gump', year: 1994 },
-    { title: 'Inception', year: 2010 },
-    {
-      title: 'The Lord of the Rings: The Two Towers',
-      year: 2002,
-    },
-    { title: "One Flew Over the Cuckoo's Nest", year: 1975 },
-    { title: 'Goodfellas', year: 1990 },
-    { title: 'The Matrix', year: 1999 },
-    { title: 'Seven Samurai', year: 1954 },
-    {
-      title: 'Star Wars: Episode IV - A New Hope',
-      year: 1977,
-    },
-    { title: 'City of God', year: 2002 },
-    { title: 'Se7en', year: 1995 },
-    { title: 'The Silence of the Lambs', year: 1991 },
-    { title: "It's a Wonderful Life", year: 1946 },
-    { title: 'Life Is Beautiful', year: 1997 },
-    { title: 'The Usual Suspects', year: 1995 },
-    { title: 'Léon: The Professional', year: 1994 },
-    { title: 'Spirited Away', year: 2001 },
-    { title: 'Saving Private Ryan', year: 1998 },
-    { title: 'Once Upon a Time in the West', year: 1968 },
-    { title: 'American History X', year: 1998 },
-    { title: 'Interstellar', year: 2014 },
-    { title: 'Casablanca', year: 1942 },
-    { title: 'City Lights', year: 1931 },
-    { title: 'Psycho', year: 1960 },
-    { title: 'The Green Mile', year: 1999 },
-    { title: 'The Intouchables', year: 2011 },
-    { title: 'Modern Times', year: 1936 },
-    { title: 'Raiders of the Lost Ark', year: 1981 },
-    { title: 'Rear Window', year: 1954 },
-    { title: 'The Pianist', year: 2002 },
-    { title: 'The Departed', year: 2006 },
-    { title: 'Terminator 2: Judgment Day', year: 1991 },
-    { title: 'Back to the Future', year: 1985 },
-    { title: 'Whiplash', year: 2014 },
-    { title: 'Gladiator', year: 2000 },
-    { title: 'Memento', year: 2000 },
-    { title: 'The Prestige', year: 2006 },
-    { title: 'The Lion King', year: 1994 },
-    { title: 'Apocalypse Now', year: 1979 },
-    { title: 'Alien', year: 1979 },
-    { title: 'Sunset Boulevard', year: 1950 },
-    {
-      title: 'Dr. Strangelove or: How I Learned to Stop Worrying and Love the Bomb',
-      year: 1964,
-    },
-    { title: 'The Great Dictator', year: 1940 },
-    { title: 'Cinema Paradiso', year: 1988 },
-    { title: 'The Lives of Others', year: 2006 },
-    { title: 'Grave of the Fireflies', year: 1988 },
-    { title: 'Paths of Glory', year: 1957 },
-    { title: 'Django Unchained', year: 2012 },
-    { title: 'The Shining', year: 1980 },
-    { title: 'WALL·E', year: 2008 },
-    { title: 'American Beauty', year: 1999 },
-    { title: 'The Dark Knight Rises', year: 2012 },
-    { title: 'Princess Mononoke', year: 1997 },
-    { title: 'Aliens', year: 1986 },
-    { title: 'Oldboy', year: 2003 },
-    { title: 'Once Upon a Time in America', year: 1984 },
-    { title: 'Witness for the Prosecution', year: 1957 },
-    { title: 'Das Boot', year: 1981 },
-    { title: 'Citizen Kane', year: 1941 },
-    { title: 'North by Northwest', year: 1959 },
-    { title: 'Vertigo', year: 1958 },
-    {
-      title: 'Star Wars: Episode VI - Return of the Jedi',
-      year: 1983,
-    },
-    { title: 'Reservoir Dogs', year: 1992 },
-    { title: 'Braveheart', year: 1995 },
-    { title: 'M', year: 1931 },
-    { title: 'Requiem for a Dream', year: 2000 },
-    { title: 'Amélie', year: 2001 },
-    { title: 'A Clockwork Orange', year: 1971 },
-    { title: 'Like Stars on Earth', year: 2007 },
-    { title: 'Taxi Driver', year: 1976 },
-    { title: 'Lawrence of Arabia', year: 1962 },
-    { title: 'Double Indemnity', year: 1944 },
-    {
-      title: 'Eternal Sunshine of the Spotless Mind',
-      year: 2004,
-    },
-    { title: 'Amadeus', year: 1984 },
-    { title: 'To Kill a Mockingbird', year: 1962 },
-    { title: 'Toy Story 3', year: 2010 },
-    { title: 'Logan', year: 2017 },
-    { title: 'Full Metal Jacket', year: 1987 },
-    { title: 'Dangal', year: 2016 },
-    { title: 'The Sting', year: 1973 },
-    { title: '2001: A Space Odyssey', year: 1968 },
-    { title: "Singin' in the Rain", year: 1952 },
-    { title: 'Toy Story', year: 1995 },
-    { title: 'Bicycle Thieves', year: 1948 },
-    { title: 'The Kid', year: 1921 },
-    { title: 'Inglourious Basterds', year: 2009 },
-    { title: 'Snatch', year: 2000 },
-    { title: '3 Idiots', year: 2009 },
-    { title: 'Monty Python and the Holy Grail', year: 1975 },
+      title: 'ReactJs'},
+    { title: 'NodeJs'},
   ];
 
+  const profileData:Profile[] = [
+    {
+      id: 1,
+      name: 'John Doe',
+      description: 'Web Developer',
+      photo: profile,
+    },
+    {
+      id: 2,
+      name: 'Jane Smith',
+      description: 'UX Designer',
+      photo: profile,
+    },
+    {
+      id: 3,
+      name: 'Bob Johnson',
+      description: 'Software Engineer',
+      photo: profile,
+    },
+    {
+      id: 4,
+      name: 'Alice Brown',
+      description: 'Data Scientist',
+      photo: profile,
+    },
+  ];  
+
 const FindSpecialist = () => {
+  const [selectedOptions, setSelectedOptions] = useState<Profile[]>([]);
+  const [showProfileCards, setShowProfileCards] = useState(true);
+
+  const loadProfileCards = () => {
+    setSelectedOptions(profileData);
+    setShowProfileCards(false);
+  };
 
   return (
-    <div>
-      <div className='Container'>
-        <div className='Content'>
-         <Stack spacing={3} sx={{ width: 500 }}>
-            <Autocomplete
-              multiple
-              id="tags-outlined"
-              options={top100Films}
-              getOptionLabel={(option) => option.title}
-              defaultValue={[top100Films[13]]}
-              filterSelectedOptions
-              renderInput={(params) => (
-                <TextField
-                  {...params}
-                  label="filterSelectedOptions"
-                  placeholder="Favorites"
-                />
-              )}
-            />
-          </Stack>
-        </div>
+    <div className='Container'>
+      <div className='container'>{showProfileCards? (
+              <div className='Content'>
+                 <Stack spacing={3} sx={{ width: 600 }}>
+                   <Autocomplete
+                     multiple
+                     id="tags-outlined"
+                     options={top100Films}
+                     getOptionLabel={(option) => option.title}
+                     defaultValue={[top100Films[0]]}
+                     filterSelectedOptions
+                     renderInput={(params) => (
+                       <TextField
+                         {...params}
+                         label="Select Technologies"
+                         placeholder="Favorites"
+                       />
+                     )}
+                   />
+                 </Stack>
+                 <Button className='searchbutton' variant="contained" size='medium' onClick={loadProfileCards}>
+                   <FaSearch></FaSearch>
+                 </Button>
+               </div>
+      ): (
+          <div className='profile-card'>
+              {selectedOptions.map((option) => (
+                <Card key={option.id} className='profile-card-item'>
+                  <CardContent>
+                    <img src={option.photo} alt={option.name} style={{ maxWidth: '100px' }} className='profile-photo'/>
+                    <Typography variant="h5">{option.name}</Typography>
+                    <Typography variant="body2">{option.description}</Typography>
+                  </CardContent>
+                </Card>
+              ))}
+          </div>
+        )}
       </div>
     </div>
   )
